6dd00491270fe992fb1bf0fe7adf79556b83bc53
Ayana Powell <ayanapowell1@gmail.com>
2016-11-04 10:07:57 -0700

    add moment.js to display moment-from-now on posts

--- app/components/add-post.js
+++ app/components/add-post.js
@@ -3,14 +3,14 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     savePost() {
-
       var params = {
         username: this.get('username'),
         title: this.get('title'),
-        content: this.get('content')
+        content: this.get('content'),
+        date: this.get('date')
       };
       if(params['content'] === "") {
-        alert('please fill out all fields')
+        alert('please fill out all fields');
       }
       $('.small.modal').modal('hide');
       $('#add-post').trigger('reset');
